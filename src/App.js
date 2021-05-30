0b8350600ec58c7101084ceefffd2d47e3346479
wesleysan7os <wesley.santos.silva@ccc.ufcg.edu.br>
2021-05-30 15:26:32 -0300

    adicionando tasks

--- src/App.js
+++ src/App.js
@@ -1,4 +1,5 @@
 import { useState } from "react";
+import AddTask from "./components/AddTask";
 import Header from "./components/Header";
 import Tasks from "./components/Tasks";
 
@@ -16,6 +17,12 @@ function App() {
       "day": "Feb 6th at 1:30pm",
       "reminder": true
     },
+    {
+      "id": 3,
+      "text": "Food Shopping",
+      "day": "Feb 5th at 2:30pm",
+      "reminder": false
+    }
 
   ]);
 
@@ -23,10 +30,25 @@ function App() {
     setTasks(tasks.filter((task) => task.id !== id));
   }
 
+  const toggleReminder = (id) => {
+    setTasks(
+      tasks.map((task) => 
+        task.id === id ? { ...task, reminder: !task.reminder } : task
+      )
+    )
+  }
+
+  const addTask = (task) => {
+    const id = Math.floor(Math.random() * 10000) + 1;
+    const newTask = { id, ...task };
+    setTasks([...tasks, newTask]);
+  }
+
   return (
     <div className="container">
       <Header title="Task Tracker" />
-      {tasks.length > 0 ? <Tasks tasks={tasks} onDelete={deleteTask}/> : 'No Tasks To Show' }
+      <AddTask onAdd={addTask}/>
+      {tasks.length > 0 ? <Tasks tasks={tasks} onDelete={deleteTask} onToggle={toggleReminder}/> : 'No Tasks To Show' }
     </div>
   );
 }
